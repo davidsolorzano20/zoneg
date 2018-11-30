eccae82e78daea219b5a89cd9506d1e70baa28ef
Luis Solorzano <luisnic20@gmail.com>
2018-11-30 12:58:57 -0600

    project

---
+++ precache-manifest.11ff8ca53849d27c6134a4e88df7bba9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4dbcdad77cff9924e51f",
+    "url": "/static/js/main.4dbcdad7.chunk.js"
+  },
+  {
+    "revision": "ffcb3a957a4566d84998",
+    "url": "/static/js/1.ffcb3a95.chunk.js"
+  },
+  {
+    "revision": "621b2a824b11c3020bfb36b12fdff279",
+    "url": "/index.html"
+  }
+];
